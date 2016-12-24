2cbc25a2b3a4542dde8113d4c6c242337df73844
Bartosz Kijanka <duelbart5@gmail.com>
2016-12-24 01:59:38 +0100

    Add JavaFX skeleton and run task to gradle

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("CalorieTracker");
+        stage.show();
+    }
+}
