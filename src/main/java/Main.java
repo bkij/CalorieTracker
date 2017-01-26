1636b0548fd664935f90cdbad31ecdffed76fa0d
Bartosz Kijanka <duelbart5@gmail.com>
2017-01-26 21:37:43 +0100

    Add important TODO to start

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,10 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
+    public void init() {
+        //TODO: Add info parsing on first use(?) and possibly database connection
+    }
+    @Override
     public void start(Stage stage) {
         stage.setTitle("CalorieTracker");
         stage.show();
