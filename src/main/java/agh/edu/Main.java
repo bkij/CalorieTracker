29d3e03983ddec7eb8b97763dced06213096488b
Bartosz Kijanka <duelbart5@gmail.com>
2017-01-26 21:48:55 +0100

    Add other TODOs related to layouts and create a base package

--- src/main/java/agh/edu/Main.java
+++ src/main/java/agh/edu/Main.java
@@ -1,3 +1,5 @@
+package agh.edu;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -8,6 +10,8 @@ public class Main extends Application {
     }
     @Override
     public void start(Stage stage) {
+        //TODO: VBox for menu buttons
+        //TODO: Flow pane for main windows
         stage.setTitle("CalorieTracker");
         stage.show();
     }
