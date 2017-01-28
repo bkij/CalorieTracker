b34f30d39605a2e35bd49edaa88db4e298417ff6
Bartosz Kijanka <duelbart5@gmail.com>
2017-01-28 15:41:20 +0100

    Add skelethon concrete persistence

---
+++ src/main/java/agh/edu/persistence/inMemory/FoodInfoAggregator.java
@@ -0,0 +1,6 @@
+package agh.edu.persistence.inMemory;
+
+import agh.edu.persistence.FoodInfoPersistence;
+
+public class FoodInfoAggregator implements FoodInfoPersistence {
+}
---
+++ src/main/java/agh/edu/persistence/inMemory/StatisticAggregator.java
@@ -0,0 +1,6 @@
+package agh.edu.persistence.inMemory;
+
+import agh.edu.persistence.StatisticPersistence;
+
+public class StatisticAggregator implements StatisticPersistence {
+}
