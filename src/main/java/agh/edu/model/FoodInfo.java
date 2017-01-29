e30019393cb7ef9e65b13b06212bf16d1e8890b6
Bartosz Kijanka <duelbart5@gmail.com>
2017-01-29 01:41:19 +0100

    Finish FoodInfo

--- src/main/java/agh/edu/model/FoodInfo.java
+++ src/main/java/agh/edu/model/FoodInfo.java
@@ -15,4 +15,218 @@ public class FoodInfo implements Serializable {
     private double E;
     private double Calcium;
     private double Choline;
+    private double Copper;
+    private double B12;
+    private double Iron;
+    private double Magnesium;
+    private double Manganese;
+    private double B3;
+    private double B5;
+    private double Phosphorus;
+    private double Potassium;
+    private double B2;
+    private double Selenium;
+    private double Sodium;
+    private double B1;
+    private double Zinc;
+
+    public double getProtein() {
+        return protein;
+    }
+
+    public void setProtein(double protein) {
+        this.protein = protein;
+    }
+
+    public double getCarbs() {
+        return carbs;
+    }
+
+    public void setCarbs(double carbs) {
+        this.carbs = carbs;
+    }
+
+    public double getFat() {
+        return fat;
+    }
+
+    public void setFat(double fat) {
+        this.fat = fat;
+    }
+
+    public double getA() {
+        return A;
+    }
+
+    public void setA(double a) {
+        A = a;
+    }
+
+    public double getC() {
+        return C;
+    }
+
+    public void setC(double c) {
+        C = c;
+    }
+
+    public double getD() {
+        return D;
+    }
+
+    public void setD(double d) {
+        D = d;
+    }
+
+    public double getK() {
+        return K;
+    }
+
+    public void setK(double k) {
+        K = k;
+    }
+
+    public double getB6() {
+        return B6;
+    }
+
+    public void setB6(double b6) {
+        B6 = b6;
+    }
+
+    public double getE() {
+        return E;
+    }
+
+    public void setE(double e) {
+        E = e;
+    }
+
+    public double getCalcium() {
+        return Calcium;
+    }
+
+    public void setCalcium(double calcium) {
+        Calcium = calcium;
+    }
+
+    public double getCholine() {
+        return Choline;
+    }
+
+    public void setCholine(double choline) {
+        Choline = choline;
+    }
+
+    public double getCopper() {
+        return Copper;
+    }
+
+    public void setCopper(double copper) {
+        Copper = copper;
+    }
+
+    public double getB12() {
+        return B12;
+    }
+
+    public void setB12(double b12) {
+        B12 = b12;
+    }
+
+    public double getIron() {
+        return Iron;
+    }
+
+    public void setIron(double iron) {
+        Iron = iron;
+    }
+
+    public double getMagnesium() {
+        return Magnesium;
+    }
+
+    public void setMagnesium(double magnesium) {
+        Magnesium = magnesium;
+    }
+
+    public double getManganese() {
+        return Manganese;
+    }
+
+    public void setManganese(double manganese) {
+        Manganese = manganese;
+    }
+
+    public double getB3() {
+        return B3;
+    }
+
+    public void setB3(double b3) {
+        B3 = b3;
+    }
+
+    public double getB5() {
+        return B5;
+    }
+
+    public void setB5(double b5) {
+        B5 = b5;
+    }
+
+    public double getPhosphorus() {
+        return Phosphorus;
+    }
+
+    public void setPhosphorus(double phosphorus) {
+        Phosphorus = phosphorus;
+    }
+
+    public double getPotassium() {
+        return Potassium;
+    }
+
+    public void setPotassium(double potassium) {
+        Potassium = potassium;
+    }
+
+    public double getB2() {
+        return B2;
+    }
+
+    public void setB2(double b2) {
+        B2 = b2;
+    }
+
+    public double getSelenium() {
+        return Selenium;
+    }
+
+    public void setSelenium(double selenium) {
+        Selenium = selenium;
+    }
+
+    public double getSodium() {
+        return Sodium;
+    }
+
+    public void setSodium(double sodium) {
+        Sodium = sodium;
+    }
+
+    public double getB1() {
+        return B1;
+    }
+
+    public void setB1(double b1) {
+        B1 = b1;
+    }
+
+    public double getZinc() {
+        return Zinc;
+    }
+
+    public void setZinc(double zinc) {
+        Zinc = zinc;
+    }
 }
