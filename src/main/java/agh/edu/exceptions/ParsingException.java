89fa9e87833d40e95641402116455946c7f113c4
Bartosz Kijanka <duelbart5@gmail.com>
2017-01-29 02:21:52 +0100

    Add usda file parsing

---
+++ src/main/java/agh/edu/exceptions/ParsingException.java
@@ -0,0 +1,10 @@
+package agh.edu.exceptions;
+
+public class ParsingException extends RuntimeException {
+    public ParsingException() {
+        super();
+    }
+    public ParsingException(String message) {
+        super(message);
+    }
+}
--- src/main/java/agh/edu/parsers/USDAParser.java
+++ src/main/java/agh/edu/parsers/USDAParser.java
@@ -1,15 +1,80 @@
 package agh.edu.parsers;
 
+import agh.edu.exceptions.ParsingException;
 import agh.edu.model.FoodInfo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.List;
 
 public class USDAParser implements NutritionalDataParser {
+    private final int numNutritionFields = 51;
     @Override
     public List<FoodInfo> parse(String filesPath) {
         String fileName = "ABBREV.txt";
         List<FoodInfo> foodData = new LinkedList<>();
 
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(filesPath + fileName);
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+            String currentLine = reader.readLine();
+            FoodInfo currentFoodInfo = getInfoForCurrentLine(currentLine);
+            foodData.add(currentFoodInfo);
+
+        } catch(IOException ex) {
+            //TODO: Change?
+            throw new ParsingException("Fatal error - IOException");
+        }
+
+        return foodData;
+    }
+
+    private FoodInfo getInfoForCurrentLine(String currentLine) {
+        FoodInfo currentFoodInfo = new FoodInfo();
+
+        String[] parts = currentLine.split("[^]");
+        double[] nutInfo = new double[parts.length - 2];
+
+        for(int i = 0; i < nutInfo.length; i++) {
+            nutInfo[i] = Double.parseDouble(parts[i + 2]);
+        }
+        if(nutInfo.length != numNutritionFields) {
+            throw new ParsingException("File format problem");
+        }
+        setNutritionalInfo(currentFoodInfo, nutInfo, parts[1]);
+
+        return currentFoodInfo;
+    }
+
+    private void setNutritionalInfo(FoodInfo info, double[] nutInfo, String foodName) {
+        info.setName(foodName);
+        info.setKcal(nutInfo[1]);
+        info.setProtein(nutInfo[2]);
+        info.setFat(nutInfo[3]);
+        info.setCarbs(nutInfo[5]);
+        info.setCarbs(nutInfo[8]);
+        info.setIron(nutInfo[9]);
+        info.setMagnesium(nutInfo[10]);
+        info.setPhosphorus(nutInfo[11]);
+        info.setPotassium(nutInfo[12]);
+        info.setSodium(nutInfo[13]);
+        info.setZinc(nutInfo[14]);
+        info.setCopper(nutInfo[15]);
+        info.setManganese(nutInfo[16]);
+        info.setSelenium(nutInfo[17]);
+        info.setC(nutInfo[18]);
+        info.setB1(nutInfo[19]);
+        info.setB2(nutInfo[20]);
+        info.setB3(nutInfo[21]);
+        info.setB5(nutInfo[22]);
+        info.setB6(nutInfo[23]);
+        info.setCholine(nutInfo[28]);
+        info.setB12(nutInfo[29]);
+        info.setA(nutInfo[31]);
+        info.setE(nutInfo[38]);
+        info.setD(nutInfo[39]);
+        info.setK(nutInfo[41]);
     }
 }
