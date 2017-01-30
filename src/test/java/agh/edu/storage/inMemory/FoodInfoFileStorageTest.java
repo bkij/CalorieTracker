4ec7b4f0823cb6fe90d85cf9d9ee7842b637dfd3
Bartosz Kijanka <duelbart5@gmail.com>
2017-01-30 20:46:09 +0100

    Finalize storage tests for now

--- src/test/java/agh/edu/storage/inMemory/FoodInfoFileStorageTest.java
+++ src/test/java/agh/edu/storage/inMemory/FoodInfoFileStorageTest.java
@@ -2,13 +2,14 @@ package agh.edu.storage.inMemory;
 
 import org.junit.Test;
 
-//TODO: Add more tests
+//TODO: Add more tests if time allows
 public class FoodInfoFileStorageTest {
     private FoodInfoFileStorage foodInfoFileStorage;
 
     @Test
     public void testNoExceptionsOnNoFilePresent() {
         foodInfoFileStorage = new FoodInfoFileStorage("testPersistenceData/", "hihi");
+        foodInfoFileStorage.initializeStorage();
     }
 
 
